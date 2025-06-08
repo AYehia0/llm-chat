bc30f82e18273e561009971ab02141ef312163ce
AYehia0 <ahmedyehia611@gmail.com>
2025-06-08 21:10:51 +0300

    chore: add some next configs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: 'export', // Enable static export; use `next build && next export` to generate static files
   distDir: 'dist', // Output directory for the static files
+  basePath: '/llm-chat', // Base path for the application
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 }
 
 module.exports = nextConfig
